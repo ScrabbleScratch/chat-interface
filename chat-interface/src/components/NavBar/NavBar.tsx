7149835f38ee24b6d44f59ddd43dbfb02cd0e54c
Mario Orellana <mario@journeyhero.net>
2023-05-17 16:45:59 -0600

    Add logo placeholder

--- chat-interface/src/components/NavBar/NavBar.tsx
+++ chat-interface/src/components/NavBar/NavBar.tsx
@@ -5,6 +5,7 @@ type NavBarProps = object;
 const NavBar: React.FC<NavBarProps> = () => {
     return (
         <div id="nav-bar">
+            <img id="title-logo" src="https://static-00.iconduck.com/assets.00/placeholder-icon-369x512-yy0egg4a.png" alt="logo" />
             <p id="title">CHAT WEB APP</p>
         </div>
     );
